745f853819fe2ccc4f17c32c379c5b1fc0c1f446
Karol <karol.gozdziewski200@gmail.com>
2024-12-02 19:22:10 +0100

    zmiana metody z void na double int

--- CalkaRownolegleKG/Program.cs
+++ CalkaRownolegleKG/Program.cs
@@ -32,28 +32,43 @@ namespace CalkaRownolegleKG
                     break;
                 }
 
-                IFunkcja funkcja = choice switch{
+                IFunkcja funkcja = choice switch
+                {
                     "1" => new Funkcja1(),
                     "2" => new Funkcja2(),
-                    "3" => new Funkcja3(), };
-                   
+                    "3" => new Funkcja3(),
+                };
+
                 ParametryDoCalki parametry = new ParametryDoCalki();
-                Console.WriteLine("Podaj ilość podziałów [ile całek chcesz obliczyć]: ");
+                Console.WriteLine("\nPodaj ilość podziałów [ile całek chcesz obliczyć]: ");
                 //parametry.podzialy = int.Parse(Console.ReadLine());
                 parametry.podzialy = 3;
-                for(int i = 0; i < parametry.podzialy; i++) {
-                    Console.WriteLine($"Podaj zakresy {i + 1} (format: [Początek całkowania,Koniec całkowania]  )");
-                    var wpisane_zakresy = Console.ReadLine().Split(new[] {',', ' '},
-                                                                   StringSplitOptions.RemoveEmptyEntries );
+                for (int i = 0; i < parametry.podzialy; i++)
+                {
+                    Console.WriteLine($"\nPodaj zakresy {i + 1} (format: [Początek całkowania,Koniec całkowania]  )");
+                    var wpisane_zakresy = Console.ReadLine().Split(new[] { ',', ' ' },
+                                                                   StringSplitOptions.RemoveEmptyEntries);
                     int x = int.Parse(wpisane_zakresy[0]);
-                    int y = int.Parse(wpisane_zakresy [1]);
+                    int y = int.Parse(wpisane_zakresy[1]);
                     //int iloscprzedzialow = int.Parse(wpisane_zakresy[2]);
                     parametry.ZakresyCalki.Add(new Tuple<int, int>(x, y));
                 }
 
                 KalkulatorTrapez kalkulatorTra = new KalkulatorTrapez();
-                kalkulatorTra.metodaTrapezow(parametry, funkcja);
-                
+                List<(int, double)> wynikicalek = kalkulatorTra.metodaTrapezow(parametry, funkcja);
+
+                Console.WriteLine("\nPodsumowanie:");
+                Console.WriteLine($"Funkcja: {funkcja.GetType().Name}");
+                for (int i = 0; i < parametry.ZakresyCalki.Count; i++) { 
+
+                    Console.WriteLine($"Przedział{i+1}: {parametry.ZakresyCalki[i]}");
+                }
+                foreach (var wynik in wynikicalek)
+                {
+                    
+                    Console.WriteLine($"Całka#{wynik.Item1}=== {wynik.Item2}");
+                }
+                Console.WriteLine($"Suma wynikow: {wynikicalek.Sum(WynikSumy => WynikSumy.Item2)}");
 
             }
         }
